c624a1234eb8e85d22efdfe3a0b4f838cd4f8938
paul <akulich.p@gmail.com>
2019-07-11 23:40:42 +0300

    password key working

--- src/test/password-key.ts
+++ src/test/password-key.ts
@@ -1,4 +1,8 @@
-import { generatePasswordKeyAsString } from 'utils/password-utils/derive-key.password-utils';
+import {
+  generatePasswordKeyAsString,
+  importPasswordKeyFromString,
+  exportPasswordKey,
+} from 'utils/password-utils/derive-key.password-utils';
 
 export const testKeyGeneration = async () => {
   const passwordString = '123';
@@ -9,9 +13,36 @@ export const testKeyGeneration = async () => {
   const passwordKeyAgain = await generatePasswordKeyAsString(
     passwordStringAgain
   );
-  debugger;
+
   console.log('passwordKeyAgain', passwordKeyAgain);
   console.log('isEquals', passwordKeyAgain === passwordKey);
+
+  if (typeof passwordKeyAgain === 'string' && typeof passwordKey === 'string') {
+    const resultedKeyImported = await importPasswordKeyFromString(passwordKey);
+    const resultedKeyImportedAgain = await importPasswordKeyFromString(
+      passwordKeyAgain
+    );
+
+    if (
+      !(resultedKeyImported instanceof Error) &&
+      !(resultedKeyImported instanceof Error)
+    ) {
+      const exportedImported = await exportPasswordKey(resultedKeyImported);
+      const exportedAgainImported = await exportPasswordKey(
+        resultedKeyImported
+      );
+
+      console.log('exportedImported', exportedImported);
+      console.log('exportedAgainImported', exportedAgainImported);
+
+      if (
+        !(exportedImported instanceof Error) &&
+        !(exportedAgainImported instanceof Error)
+      ) {
+        console.log('isEquals', exportedImported.k === exportedAgainImported.k);
+      }
+    }
+  }
 };
 
 testKeyGeneration();
