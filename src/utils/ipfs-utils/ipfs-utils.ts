5931503367075ccd8b342e21284e6baa95d73cd7
Paul Akulich <akulich.p@gmail.com>
2020-08-01 00:37:44 +0400

    upgraded ipfs

--- src/utils/ipfs-utils/ipfs-utils.ts
+++ src/utils/ipfs-utils/ipfs-utils.ts
@@ -17,7 +17,48 @@ export const ipfsUtilsConnectBasic = async (
     timer = setTimeout(() => {
       throw new Error('Connection timed out');
     }, timeoutMs);
-    return await IPFS.create();
+    const ipfs = await IPFS.create({
+      config: {
+        Bootstrap: [],
+        Addresses: {
+          // TODO - it works for now, only for test purposes
+          Swarm: [
+            '/dns4/p2p.3box.io/tcp/9091/wss/p2p-webrtc-star/',
+            '/dns4/stardust.mkg20001.io/tcp/443/wss/p2p-stardust/',
+          ],
+        },
+      },
+    });
+
+    let idx = Math.random() * 100;
+    const topicName = 'TEST_TOPIC_TEST_______';
+    await ipfs.pubsub.subscribe(
+      topicName,
+      ({
+        from,
+        seqno,
+        data,
+      }: {
+        from: string;
+        seqno: Buffer;
+        data: Buffer;
+      }) => {
+        if (data) {
+          console.log(from);
+          console.log(data.toString());
+        }
+      }
+    );
+    setInterval(async () => {
+      if ((window as any).__sending) {
+        await ipfs.pubsub.publish(
+          'TEST_TOPIC_TEST_______',
+          `test data ${idx++}`
+        );
+      }
+    }, 1500);
+
+    return ipfs;
   } finally {
     clearTimeout(timer!);
   }
